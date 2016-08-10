b9107767659ff7a3f393a6adef0a22fb5f25747d
Stepan Suvorov <stevermeister@gmail.com>
2016-08-10 21:06:20 +0200

    initial structure

---
+++ e2e/app.po.ts
@@ -0,0 +1,9 @@
+export class DutchTaxIncomeCalculator2Page {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
