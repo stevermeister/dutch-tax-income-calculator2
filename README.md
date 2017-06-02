0131d804f58b2f64b6632c5e4a0d7ac0c24eeafb
Stepan Suvorov <stevermeister@gmail.com>
2017-06-02 21:30:32 +0200

    dummy page

--- README.md
+++
@@ -1,2 +0,0 @@
-# dutch-tax-income-calculator2
-new version of dutch-tax-income-calculator built with Angular2
--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class DutchTaxIncomeCalculator2Page {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}
